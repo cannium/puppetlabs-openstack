0acbc4a9369d3579f862fd95783982af900dea2e
Can Zhang <can@canx.me>
2014-06-03 17:49:57 +0800

    Add parameter "openstack::devmode"

--- manifests/common/nova.pp
+++ manifests/common/nova.pp
@@ -21,6 +21,7 @@ class havana::common::nova ($is_compute = false) {
     rabbit_password    => hiera('openstack::rabbitmq::password'),
     debug              => hiera('openstack::debug'),
     verbose            => hiera('openstack::verbose'),
+    devmode            => hiera('openstack::devmode'),
   }
 
   nova_config { 'DEFAULT/default_floating_pool': value => 'public' }

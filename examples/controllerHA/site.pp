4cb05d225e947d1d0fc097166c4aba84c13837e6
Can Zhang <can@canx.me>
2014-06-06 10:02:00 +0800

    finalize example config files

--- examples/controllerHA/site.pp
+++ examples/controllerHA/site.pp
@@ -5,11 +5,13 @@ node 'puppet' {
 node 'controller01' {
   include havana::resources::haproxy
   include havana::resources::keepalived
+  include havana::role::controllerHA
 }
 
 node 'controller02' {
   include havana::resources::haproxy
   include havana::resources::keepalived
+  include havana::role::controllerHA
 }
 
 node 'database' {

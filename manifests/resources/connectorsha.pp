37d3bfc218544c315689c126c26c1001459f7e44
Can Zhang <can@canx.me>
2014-06-12 17:00:04 +0800

    fix database connection

--- manifests/resources/connectorsha.pp
+++ manifests/resources/connectorsha.pp
@@ -1,14 +1,13 @@
 class havana::resources::connectorsha {
-  include havana::address
-  $management_address = $::havana::address::controller_management_address
+  $database_address = hiera('openstack::mysql::address')
   $password = hiera('openstack::mysql::service_password')
 
-  $keystone = "mysql://keystone:${password}@${management_address}/keystone"
-  $cinder   = "mysql://cinder:${password}@${management_address}/cinder"
-  $glance   = "mysql://glance:${password}@${management_address}/glance"
-  $nova     = "mysql://nova:${password}@${management_address}/nova"
-  $neutron  = "mysql://neutron:${password}@${management_address}/neutron"
-  $heat     = "mysql://heat:${password}@${management_address}/heat"
+  $keystone = "mysql://keystone:${password}@${database_address}/keystone"
+  $cinder   = "mysql://cinder:${password}@${database_address}/cinder"
+  $glance   = "mysql://glance:${password}@${database_address}/glance"
+  $nova     = "mysql://nova:${password}@${database_address}/nova"
+  $neutron  = "mysql://neutron:${password}@${database_address}/neutron"
+  $heat     = "mysql://heat:${password}@${database_address}/heat"
 
-  include mysql::config
+  include mysql::server
 }

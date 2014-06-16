fc90ed4e653219956d7bc6d6c848060264e60818
Can Zhang <can@canx.me>
2014-06-16 11:05:50 +0800

    add order(dependency) in site.pp

--- examples/controllerHA/site.pp
+++ examples/controllerHA/site.pp
@@ -3,14 +3,14 @@ node 'puppet' {
 }
 
 node 'controller01' {
-  include ::havana::resources::haproxy
-  include ::havana::resources::keepalived
+  include ::havana::resources::haproxy ->
+  include ::havana::resources::keepalived ->
   include ::havana::roleha::controller
 }
 
 node 'controller02' {
-  include ::havana::resources::haproxy
-  include ::havana::resources::keepalived
+  include ::havana::resources::haproxy ->
+  include ::havana::resources::keepalived ->
   include ::havana::roleha::controller
 }
 

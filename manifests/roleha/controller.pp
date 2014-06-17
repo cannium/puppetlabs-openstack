04252330810286631e613b768c50952092238042
Can Zhang <can@canx.me>
2014-06-17 18:05:28 +0800

    remove glance-api because it's on storage node

--- manifests/roleha/controller.pp
+++ manifests/roleha/controller.pp
@@ -3,7 +3,6 @@ class havana::roleha::controller inherits ::havana::roleha {
   class { '::havana::profile::rabbitmqha': } ->
   class { '::havana::profile::memcacheha': } ->
   class { '::havana::profile::keystoneha': } ->
-  class { '::havana::profile::glanceha::api': } ->
   class { '::havana::profile::glanceha::auth': } ->
   class { '::havana::profile::novaha::api': } ->
   class { '::havana::profile::horizonha': }

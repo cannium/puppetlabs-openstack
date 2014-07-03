cbf0d207b0d68ac9053792b7af7ca1d3722f66e7
Can Zhang <can@canx.me>
2014-07-03 16:51:19 +0800

    use vip in compute nodes for controller addressing

--- manifests/address.pp
+++ manifests/address.pp
@@ -1,17 +1,22 @@
 class havana::address {
-  $management_network = hiera('openstack::network::management')
-  $management_address = ip_for_network($management_network)
   if($::hostname == 'controller01') {
       $controller_management_address = hiera('openstack::controller::address::01')
       $storage_management_address = hiera('openstack::storage::address::01')
       $controller_api_address = hiera('openstack::controller::address::01')
       $storage_api_address    = hiera('openstack::storage::address::01')
       $other_node_address = hiera('openstack::controller::address::02')
-  } else {
+  } elsif ($::hostname == 'controller02') {
       $controller_management_address = hiera('openstack::controller::address::02')
       $storage_management_address = hiera('openstack::storage::address::02')
       $controller_api_address = hiera('openstack::controller::address::02')
       $storage_api_address    = hiera('openstack::storage::address::02')
       $other_node_address = hiera('openstack::controller::address::01')
+  } else {
+      # compute node
+      $controller_management_address = hiera('openstack::controller:address::virtual')
+      $storage_management_address = hiera('openstack::controller::address::virtual')
+      $controller_api_address = hiera('openstack::controller::address:virtual')
+      $storage_api_address = hiera('openstack::controller::address::virtual')
+      $other_node_address = hiera('openstack::controller::address::virtual')
   }
 }

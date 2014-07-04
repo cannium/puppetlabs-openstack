5e3a964b28a0106c476073946ca8b089531c0211
Can Zhang <can@canx.me>
2014-07-04 11:45:53 +0800

    rabbitmq cluster config

--- manifests/profile/rabbitmqha.pp
+++ manifests/profile/rabbitmqha.pp
@@ -2,9 +2,10 @@
 class havana::profile::rabbitmqha {
   include havana::address
   $address = $::havana::address::controller_management_address
+  $other_address = $::havana::address::other_node_address
   class { '::nova::rabbitmq':
     userid             => hiera('openstack::rabbitmq::user'),
     password           => hiera('openstack::rabbitmq::password'),
-    cluster_disk_nodes => $address,
+    cluster_disk_nodes => [$address, $other_address],
   }
 }

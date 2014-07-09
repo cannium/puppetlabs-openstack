ebb69d62fb667ce6dbc7131c0735dc2d542e9712
Can Zhang <can@canx.me>
2014-07-09 10:35:57 +0800

    add SESSION_ENGINE, CACHES parameter to horizon conf

--- manifests/profile/horizonha.pp
+++ manifests/profile/horizonha.pp
@@ -2,13 +2,17 @@
 class havana::profile::horizonha {
   include havana::address
   $address = $::havana::address::controller_management_address
+  $controller01 = hiera('openstack::controller::address::01')
+  $controller02 = hiera('openstack::controller::address::02')
+  $cache_address = [$controller01, $controller02]
   $vip = hiera('openstack::controller::address::virtual')
   class { '::horizon':
     fqdn            => [ '127.0.0.1', $address, $::fqdn, '*' ],
     secret_key      => hiera('openstack::horizon::secret_key'),
-    cache_server_ip => $address,
+    cache_server_ip => $cache_address,
     bind_address    => $address,
     keystone_url    => "http://${vip}:5000/v2.0",
+    local_settings_template => "havana/local_settings.py.erb"
   }
 
   ::havana::resources::firewall { 'Apache (Horizon)': port => '80' }

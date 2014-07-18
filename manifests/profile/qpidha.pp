e750ec020fe50c87de3b269d36d2c5a1186be447
Can Zhang <can@canx.me>
2014-07-18 17:48:50 +0800

    config qpid as "clustered"

--- manifests/profile/qpidha.pp
+++ manifests/profile/qpidha.pp
@@ -2,5 +2,6 @@ class havana::profile::qpidha {
     class{'::nova::qpid':
     	user  		=>	hiera('openstack::qpid::user'),
     	password	=>	hiera('openstack::qpid::password'),
+        clustered   =>  true,
 	}
 }

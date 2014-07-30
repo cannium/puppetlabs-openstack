d4a6138f0299aaeb58bf1fd43cb9dde03edca826
Can Zhang <can@canx.me>
2014-07-30 14:51:28 +0800

    Change default NTP server

--- manifests/profile/baseha.pp
+++ manifests/profile/baseha.pp
@@ -1,7 +1,9 @@
 # The base profile for OpenStack. Installs the repository and ntp
 class havana::profile::baseha {
   # everyone also needs to be on the same clock
-  class { '::ntp': }
+  class { '::ntp': 
+      servers   => hiera('ntp::servers'),
+  }
 
   # all nodes need the OpenStack repository
   class { '::havana::resources::repo': }
